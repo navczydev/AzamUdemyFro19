011747c5cbac49f52586d70599ab55891841c381
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2023-02-28 21:14:50 -0500

    Initial Commit

---
+++ SwiftUIAllDevices/SwiftUIAllDevicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAllDevicesApp.swift
+//  SwiftUIAllDevices
+//
+//  Created by NAVJOT SINGH on 2023-02-28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAllDevicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
