11114901b8d5b83b1961f7c040b7f6b5654d19e4
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2023-02-28 21:51:09 -0500

    project set up for multiple platforms

---
+++ SwiftUI-CoreFramework/SwiftUI_CoreFramework.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUI_CoreFramework.h
+//  SwiftUI-CoreFramework
+//
+//  Created by NAVJOT SINGH on 2023-02-28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUI_CoreFramework.
+FOUNDATION_EXPORT double SwiftUI_CoreFrameworkVersionNumber;
+
+//! Project version string for SwiftUI_CoreFramework.
+FOUNDATION_EXPORT const unsigned char SwiftUI_CoreFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUI_CoreFramework/PublicHeader.h>
+
+
---
+++ SwiftUIAll-WatchOS Watch App/SwiftUIAll_WatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAll_WatchOSApp.swift
+//  SwiftUIAll-WatchOS Watch App
+//
+//  Created by NAVJOT SINGH on 2023-02-28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAll_WatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
