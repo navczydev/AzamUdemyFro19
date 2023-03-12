cd5a25c43ed7f9afd16a18a11bc82fe09dbea219
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2023-03-11 20:46:16 -0500

    Initial Commit

---
+++ Sec20-Aniamtions/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sec20-Aniamtions
+//
+//  Created by NAVJOT SINGH on 2023-03-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sec20-Aniamtions/Sec20_AniamtionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sec20_AniamtionsApp.swift
+//  Sec20-Aniamtions
+//
+//  Created by NAVJOT SINGH on 2023-03-11.
+//
+
+import SwiftUI
+
+@main
+struct Sec20_AniamtionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
