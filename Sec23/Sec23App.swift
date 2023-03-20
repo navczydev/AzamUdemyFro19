e89cc80e3bc6910ce76e9dbe18238af5ef744c6c
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2023-03-19 22:03:29 -0400

    Initial Commit

---
+++ Sec23/Sec23App.swift
@@ -0,0 +1,17 @@
+//
+//  Sec23App.swift
+//  Sec23
+//
+//  Created by NAVJOT SINGH on 2023-03-19.
+//
+
+import SwiftUI
+
+@main
+struct Sec23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
